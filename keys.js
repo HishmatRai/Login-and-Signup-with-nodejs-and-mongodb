2f2afd583a68ed9a3becfae2f9489f65b64f9c4c
HishmatRai <hishmatrai.2018@gmail.com>
2020-07-28 11:42:51 -0700

    Login-and-Signup-with-nodejs-and-mongod

---
+++ keys.js
@@ -0,0 +1,18 @@
+// const mongoose = require('mongoose')
+// mongoose.connect("mongodb+srv://hackathon:hackathon@cluster0.olrun.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+//         useNewUrlParser: true,
+      
+//     })
+
+
+//     module.exports = mongoose
+
+
+module.exports = {
+
+    mogoUrl:"mongodb+srv://hackathon:hackathon@cluster0.olrun.mongodb.net/hackathon?retryWrites=true&w=majority", 
+    jwtkey:"hishamt"
+    }
+       
+
+ 
